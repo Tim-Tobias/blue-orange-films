f21f1d477075f6e0e8c8f7b1f8d715fbf787bc1d
fharhanamrin <fharhanamrin028@gmail.com>
2025-04-07 18:00:30 +0700

    feat(migration) : Rename project to "blue-orange-cms" and add multiple migration files for database schema including web contents, projects, project files, project teams, team names, crew roles, project categories, and workflows.

---
+++ database/migrations/2025_04_07_105300_create_workflows_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workflows', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('desc')->nullable();
+            $table->enum('type', ['1', '2'])->nullable(); 
+            $table->string('logo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workflows');
+    }
+};
