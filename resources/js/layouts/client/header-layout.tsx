71ddcbae292dc7afda611e699d00f5a2fc5709e2
nabilwafi <nabilwafi88@gmail.com>
2025-04-20 18:53:34 +0700

    branch test

---
+++ resources/js/layouts/client/header-layout.tsx
@@ -0,0 +1,12 @@
+const HeaderLayout = () => {
+    return (
+        <header className="mb-6 w-full">
+            <nav className="flex items-center justify-between gap-4">
+                <p>Logo</p>
+                <div>dawdwadaw</div>
+            </nav>
+        </header>
+    );
+};
+
+export default HeaderLayout;
