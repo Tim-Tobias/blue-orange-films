8c9e59f7024c5f256e8433e180d9e7582315d9b2
nabilwafi <nabilwafi88@gmail.com>
2025-05-03 10:06:27 +0700

    contact

--- resources/js/pages/client/contact/index.tsx
+++ resources/js/pages/client/contact/index.tsx
@@ -2,6 +2,22 @@ import { AppFrontWrapper } from '@/components/app-front-wrapper';
 import SocialMediaFooter from '@/components/social-media-footer';
 import IntroduceLayout from '@/layouts/client/IntroduceLayout';
 import { Head } from '@inertiajs/react';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+const contactList = [
+    {
+        imageUrl: 'https://picsum.photos/id/250/300/500',
+    },
+    {
+        imageUrl: 'https://picsum.photos/id/251/200/300',
+    },
+    {
+        imageUrl: 'https://picsum.photos/id/252/200/300',
+    },
+];
 
 const ContactPage = () => {
     return (
@@ -10,7 +26,17 @@ const ContactPage = () => {
 
             <IntroduceLayout imgUrl="https://picsum.photos/id/352/200/300" title="Contact" />
             <AppFrontWrapper className="mb-20">
-                <div className="grid grid-cols-1 gap-8 p-8 lg:grid-cols-2">
+                <div className="grid grid-cols-1 gap-8 p-8 lg:grid-cols-3">
+                    <div className="col-span-2 flex flex-col">
+                        <Swiper className="h-[300px] w-full" modules={[Pagination]} loop pagination={true}>
+                            {contactList.map((src, i) => (
+                                <SwiperSlide key={i}>
+                                    <img src={src.imageUrl} alt={`Slide ${i}`} className="h-full w-full object-cover" />
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+
                     <div className="flex flex-col gap-8">
                         <div>
                             <h2 className="mb-2 text-2xl font-bold text-orange-500">Talk to Us!</h2>
@@ -33,19 +59,6 @@ const ContactPage = () => {
                                 <SocialMediaFooter color="black" />
                             </div>
                         </div>
-                    </div>
-
-                    <div className="flex flex-col">
-                        <h2 className="mb-4 text-2xl font-bold text-orange-500">How to Get There</h2>
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1983.1422986339478!2d106.8299487!3d-6.3050758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f24c6e2fce9f%3A0xf3ff3d79c0a0d228!2sDAvenue%20Office%20Space!5e0!3m2!1sen!2sid!4v1651773798856!5m2!1sen!2sid"
-                            width="100%"
-                            height="250"
-                            style={{ border: 0 }}
-                            allowFullScreen={true}
-                            loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"
-                        ></iframe>
                     </div>
                 </div>
             </AppFrontWrapper>
