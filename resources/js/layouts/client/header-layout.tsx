b8350b67048b2e15cebb73dd0c955c39cf0d9746
nabilwafi <nabilwafi88@gmail.com>
2025-06-13 10:42:39 +0700

    fix

--- resources/js/layouts/client/header-layout.tsx
+++ resources/js/layouts/client/header-layout.tsx
@@ -13,19 +13,19 @@ const HeaderLayout = () => {
                     <div className="flex items-center gap-4 text-white">
                         <Link
                             href="/about"
-                            className={`text-2xl after:block after:h-[2px] after:origin-left after:scale-x-0 after:bg-white after:transition-transform after:duration-300 *:after:content-[''] hover:after:scale-x-100 ${pathname === '/about' ? 'font-semibold after:scale-x-100 after:bg-white' : ''} ${pathname === '/contact' ? 'text-black' : ''}`}
+                            className={`text-sm after:block after:h-[2px] after:origin-left after:scale-x-0 after:bg-white after:transition-transform after:duration-300 *:after:content-[''] hover:after:scale-x-100 md:text-2xl ${pathname === '/about' ? 'font-semibold after:scale-x-100 after:bg-white' : ''} ${pathname === '/contact' ? 'text-black' : ''}`}
                         >
                             About
                         </Link>
                         <Link
                             href="/works"
-                            className={`text-2xl after:block after:h-[2px] after:origin-left after:scale-x-0 after:bg-white after:transition-transform after:duration-300 *:after:content-[''] hover:after:scale-x-100 ${pathname === '/works' ? 'font-semibold after:scale-x-100 after:bg-white' : ''} ${pathname === '/contact' ? 'text-black' : ''}`}
+                            className={`text-sm after:block after:h-[2px] after:origin-left after:scale-x-0 after:bg-white after:transition-transform after:duration-300 *:after:content-[''] hover:after:scale-x-100 md:text-2xl ${pathname === '/works' ? 'font-semibold after:scale-x-100 after:bg-white' : ''} ${pathname === '/contact' ? 'text-black' : ''}`}
                         >
                             Works
                         </Link>
                         <Link
                             href="/contact"
-                            className={`text-2xl after:block after:h-[2px] after:origin-left after:scale-x-0 after:bg-white after:transition-transform after:duration-300 *:after:content-[''] hover:after:scale-x-100 ${pathname === '/contact' ? 'font-semibold text-black after:scale-x-100 after:bg-black' : ''}`}
+                            className={`text-sm after:block after:h-[2px] after:origin-left after:scale-x-0 after:bg-white after:transition-transform after:duration-300 *:after:content-[''] hover:after:scale-x-100 md:text-2xl ${pathname === '/contact' ? 'font-semibold text-black after:scale-x-100 after:bg-black' : ''}`}
                         >
                             Contact
                         </Link>
