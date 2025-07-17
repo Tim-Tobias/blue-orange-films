d30eeec09482ae7332c4984dbf9a21ac103defbe
nabilwafi <nabilwafi88@gmail.com>
2025-07-17 18:28:46 +0700

    fix footer

--- resources/js/layouts/client/footer-layout.tsx
+++ resources/js/layouts/client/footer-layout.tsx
@@ -3,7 +3,7 @@ import SocialMediaFooter from '@/components/social-media-footer';
 const FooterLayout = () => {
     return (
         <footer className="bg-[#1E4E79]">
-            <div className="mx-auto grid max-w-[1570px] items-center justify-center gap-5 p-5 md:grid-cols-2 md:justify-between">
+            <div className="mx-auto flex max-w-[1570px] flex-col items-center justify-center gap-5 p-5 md:flex-row md:justify-between">
                 <SocialMediaFooter color="white" />
 
                 <p className="text-white">© 2025 Blue Orange Films</p>
